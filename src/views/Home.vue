6ed1a0deef08385ccf657c286800ec1b0f46de1e
ryuhojin <interarea@naver.com>
2022-02-04 13:20:18 +0900

    add dayjs, lodash with treeshaking

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,14 @@
 <template>
   <div>HOME PAGE</div>
 </template>
+<script>
+import dayjs from "dayjs";
+import cloneDeep from "lodash-es/cloneDeep";
+export default {
+  mounted() {
+    const a = cloneDeep("a");
+    console.log(a)
+    console.log(dayjs('2022-02-04'))
+  },
+};
+</script>
